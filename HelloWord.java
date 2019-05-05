33e19cd46c492c8b18a8292185ff6fc99e283791
ZhangDemoSeven <1273495839@qq.com>
2019-05-05 16:28:50 +0800

    在develop分支下添加了一条输出语句

--- HelloWord.java
+++ HelloWord.java
@@ -3,5 +3,6 @@ public class HelloWorld {
 		System.out.println("hello world");
 		System.out.println("修改了HelloWorld类");
 		System.out.println("第二次修改");
+		System.out.println("在Develop分支下添加一条输出语句");
 	}
 }
