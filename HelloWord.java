7d1bda87dc9ccb20a9fb1dc142a7f26e7412d774
ZhangDemoSeven <1273495839@qq.com>
2019-05-05 14:50:13 +0800

    第二次修改HelloWord.java

--- HelloWord.java
+++ HelloWord.java
@@ -2,5 +2,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("修改了HelloWorld类");
+		System.out.println("第二次修改");
 	}
 }
